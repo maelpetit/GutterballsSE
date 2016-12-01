2e79296aed7c860d39b7e3762ceec5388d23bf6d
15002849 <15002849@i206m03.istic.univ-rennes1.fr>
2016-12-01 12:11:13 +0100

    working v2

--- src/Gutterballs.java
+++ src/Gutterballs.java
@@ -1,7 +1,7 @@
 
 public class Gutterballs {
 	
-	final static int NB_CLIENTS = 90; //Attention le nombre de clients doit etre divisible par le nombre de clients par groupe
+	final static int NB_CLIENTS = 30; //Attention le nombre de clients doit etre divisible par le nombre de clients par groupe
 	final static int MAX_MEMBERS_PER_GROUP = 5;
 	final static int NB_ALLEYS = 3;
 	private static final int NB_CASH_DESK = 3;
@@ -21,6 +21,14 @@ public class Gutterballs {
 		for(int i = 0; i < clients.length; i++){
 			clients[i] = new Client(i, bowling, manager, shoesroom);
 			clients[i].start();
+			int interval = (int) (Math.random()*50 + 50);
+			try {
+				
+				Thread.sleep(interval);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 				
 	}
